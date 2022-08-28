169aaac0623583d7510b94cae8289be1f28f368f
sabilnugraha <vshnugraha@gmail.com>
2022-08-28 12:00:23 +0700

    mmm

--- handlers/products.go
+++ handlers/products.go
@@ -125,7 +125,7 @@ func (h *productHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		Image: resp.SecureURL,
 	}
 
-	data, err := h.ProductRepository.CreateProduct(product)
+	product, err = h.ProductRepository.CreateProduct(product)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
@@ -136,7 +136,7 @@ func (h *productHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	product, _ = h.ProductRepository.GetProduct(product.ID)
 
 	w.WriteHeader(http.StatusOK)
-	response := dto.SuccessResult{Status: "success", Data: data}
+	response := dto.SuccessResult{Status: "success", Data: product}
 	json.NewEncoder(w).Encode(response)
 }
 
